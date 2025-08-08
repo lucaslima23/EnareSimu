c8c4b617714df0536ad1dc1734993d9e51a04faf
lucaslima23 <30532330+lucaslima23@users.noreply.github.com>
2025-08-08 15:52:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -655,16 +655,6 @@ function startTimer() {
     }, 1000);
 }
 
-// Função auxiliar para gerar cores aleatórias para o gráfico
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
 // Habilita o botão de início
 function enableStartButton() {
     startButton.disabled = false;
@@ -805,6 +795,7 @@ function startTimer() {
         }
     }, 1000);
 }
+
 
 
 
